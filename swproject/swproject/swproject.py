74ece5adfda7ebda339b4d15838caff348566fd6
IlseungKim <rladlftmd123@gmail.com>
2019-03-20 22:52:50 +0900

    Haar cascade, MovingAverage

--- swproject/swproject/swproject.py
+++ swproject/swproject/swproject.py
@@ -9,34 +9,34 @@ def HogDescriptor(image):
         return rects 
 def Haarcascade( frame):
     face_cascade = cv2.CascadeClassifier('haarcascade_frontface.xml')
-    body_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+    body_cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml')
     faces = face_cascade.detectMultiScale(gray,1.3,5)
     bodies = body_cascade.detectMultiScale(gray,1.1,5)
     return faces,bodies
     
-#cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 
-#while(1):
-#ret,frame = cap.read()
-frame = cv2.imread("test1.jpg",cv2.IMREAD_COLOR)
-gray = np.zeros(frame.shape)
-gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-#processing
+while(1):
+    ret,frame = cap.read()
+    #frame = cv2.imread("test1.jpg",cv2.IMREAD_COLOR)
+    gray = np.zeros(frame.shape)
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    #processing
 
-###HoG###
-#rects = HogDescriptor(gray)
-#for (x,y,w,h) in rects:
-#    cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+    ###HoG###
+    #rects = HogDescriptor(gray)
+    #for (x,y,w,h) in rects:
+    #    cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
-###Haar###
-faces,bodies = Haarcascade(gray)
-for (x,y,w,h) in faces:
-    cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-for (x,y,w,h) in bodies:
-    cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+    ###Haar###
+    faces,bodies = Haarcascade(gray)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+    for (x,y,w,h) in bodies:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
 
-cv2.imshow("captured",frame)
-cv2.waitKey(0)
-#if cv2.waitKey(1) == 99:
-#    break
-#cv2.destroyAllWindows()
+    cv2.imshow("captured",frame)
+    if cv2.waitKey(1) == 99:
+        break
+    #cv2.waitKey(0)
+cv2.destroyAllWindows()
