541cd71eef9a1323dbeb4cbd9db8b72911775a36
IlseungKim <rladlftmd123@gmail.com>
2019-03-25 00:09:32 +0900

    swproject body and face detection

--- swproject/swproject/swproject.py
+++ swproject/swproject/swproject.py
@@ -2,35 +2,30 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-def HogDescriptor(image):
-        hog = cv2.HOGDescriptor()
-        hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-        (rects, weights) = hog.detectMultiScale(image, winStride=(5,5),padding=(16,16), scale=1.05, useMeanshiftGrouping=False)
-        return rects 
 def Haarcascade( frame):
     face_cascade = cv2.CascadeClassifier('haarcascade_frontface.xml')
-    body_cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml')
-    faces = face_cascade.detectMultiScale(gray,1.3,5)
-    bodies = body_cascade.detectMultiScale(gray,1.03,5)
+    body_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+
+    faces = np.array(face_cascade.detectMultiScale(gray,1.3,5,minSize=(15,40), maxSize=(300,400)))
+
+    bodies = np.array(body_cascade.detectMultiScale(gray,1.03,5,minSize=(100,200), maxSize=(300,400)))
+    bodies = np.append(bodies,body_cascade.detectMultiScale(gray,1.1,5,minSize=(100,200), maxSize=(300,400)))
+    bodies = np.append(bodies,body_cascade.detectMultiScale(gray,1.3,5,minSize=(100,200), maxSize=(300,400)))
+
+    faces = np.reshape(faces,(faces.size,1))
+    bodies = np.reshape(bodies,(bodies.size,1))
     return faces,bodies
     
 cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+ret,frame = cap.read()
+print(frame.shape)
 
-<<<<<<< HEAD
 while(1):
     ret,frame = cap.read()
     #frame = cv2.imread("test1.jpg",cv2.IMREAD_COLOR)
     gray = np.zeros(frame.shape)
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     #processing
-=======
-#while(1):
-#ret,frame = cap.read()
-frame = cv2.imread("test.jpg",cv2.IMREAD_COLOR)
-gray = np.zeros(frame.shape)
-gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-#processing
->>>>>>> 8c67efc67b12d578d8346cf855093976d2bd2926
 
     ###HoG###
     #rects = HogDescriptor(gray)
@@ -39,21 +34,25 @@ gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
     ###Haar###
     faces,bodies = Haarcascade(gray)
-    for (x,y,w,h) in faces:
+    for i in range(0,faces.size,4):
+        x = int(faces[i])
+        y = int(faces[i+1])
+        w = int(faces[i+2])
+        h = int(faces[i+3])
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-    for (x,y,w,h) in bodies:
+    #for (x,y,w,h) in faces:
+    #    cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+    for i in range(0,bodies.size,4):
+        x = int(bodies[i])
+        y = int(bodies[i+1])
+        w = int(bodies[i+2])
+        h = int(bodies[i+3])
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
 
-<<<<<<< HEAD
     cv2.imshow("captured",frame)
-    if cv2.waitKey(1) == 99:
+    if cv2.waitKey(1) == 'q':
+        print("End")
         break
     #cv2.waitKey(0)
+
 cv2.destroyAllWindows()
-=======
-cv2.imshow("captured",frame)
-cv2.waitKey(0)
-#if cv2.waitKey(1) == 99:
-#    break
-#cv2.destroyAllWindows()
->>>>>>> 8c67efc67b12d578d8346cf855093976d2bd2926
